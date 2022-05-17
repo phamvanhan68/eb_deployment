7b97416df3e0162e79bdd4f62edb76161ca47d42
Han <=>
2022-05-17 20:00:25 +0700

    testing 7

--- ggcrawler_cronjob_sha.sh
+++ ggcrawler_cronjob_sha.sh
@@ -3,7 +3,4 @@ PYTHON_FILE=$(find /var/app/current -name "update_sha.py")
 # Need to find it cause this shell script will be called an run at nother directory, not here
 echo "$PYTHON_FILE"
 
-cd /var/app/current
-source venv/bin/activate
-
 python3 "$PYTHON_FILE"
--- ggcrawler_permissions.sh
+++
@@ -1,9 +0,0 @@
-sudo chmod 777 /var/app/current
-sudo chmod 777 /var/app/current/logs
-
-cd /var/app/current
-virtualenv -p python3 /var/app/current/venv
-source /var/app/current/venv/bin/activate
-pip install -r requirements.txt 
-
-
